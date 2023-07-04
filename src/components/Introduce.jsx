177d3d6d9d029e8aeea7e7fbf717117894cf7960
KIMPPT <dudghks9303@gmail.com>
2023-07-04 23:34:16 +0900

    07042334

--- src/components/Introduce.jsx
+++ src/components/Introduce.jsx
@@ -6,9 +6,9 @@ export default function Introduce() {
       <h1>자기소개</h1>
       <div className={intro["context"]}>
         <img src="" alt="증명사진자리입니다" className={intro["image"]} />
-        <ul>
-          <li>이름</li>
-          <li>생년월일</li>
+        <ul className={intro['profile']}>
+          <li>이름 : </li>
+          <li>생년월일 : </li>
           <li>경력 : 신입</li>
         </ul>
       </div>
