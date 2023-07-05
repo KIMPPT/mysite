57b348f3b99e0589a76deaaf063418cdd2ddb182
KIMPPT <dudghks9303@gmail.com>
2023-07-05 23:56:47 +0900

    07052356

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -6,14 +6,30 @@ export default function Detail() {
     <div className={detail["main"]}>
       <h3> 사용가능 기능들</h3>
       <div className={detail['available']}>
+        <span>사용가능기능</span>
+        <span>내용</span>
+        <span>사용가능기능</span>
+        <span>내용</span>
         <span>HTML</span>
-        <span>HTML</span>
-        <span>HTML</span>
-        <span>HTML</span>
-        <span>HTML</span>
-        <span>HTML</span>
-        <span>HTML</span>
-        <span>HTML</span>
+        <span>용도에 맞는 태그 사용 가능</span>
+        <span>CSS</span>
+        <span>조건에 따르 CSS 사용 가능</span>
+        <span>Javascript</span>
+        <span>조건문 사용가능</span>
+        <span>React</span>
+        <span>HOOK을 이용한 라이프 사이클 사용가능</span>        
+        <span>Redux</span>
+        <span>Store,Action함수,Reducer를 사용가능</span>        
+        <span>Typescript</span>
+        <span>타입 지정해서 쓰기 가능</span>        
+        <span>Photoshop</span>
+        <span>레이어를 나누어 작업 가능</span>        
+        <span>C++</span>
+        <span>1차원 배열까지 가능</span>
+        <span>R</span>
+        <span>기본적인 행렬 가능</span>
+        <span>OA</span>
+        <span>한글(워드),엑셀,파워포인트 기본기능 사용가능</span>
       </div>
     </div>
   );
--- src/components/Introduce.jsx
+++ src/components/Introduce.jsx
@@ -7,8 +7,8 @@ export default function Introduce() {
       <div className={intro["context"]}>
         <img src="" alt="증명사진자리입니다" className={intro["image"]} />
         <ul className={intro['profile']}>
-          <li>이름 : </li>
-          <li>생년월일 : </li>
+          <li>이름 : 김영환</li>
+          <li>생년월일 : 1993/03/18</li>
           <li>경력 : 신입</li>
         </ul>
       </div>
